a335d5f4a4fcbd141d4817e161a2feaac53cef8f
wisnu <wisnuadh@gmail.com>
2023-09-14 11:38:51 +0700

    membuat web servis komputer

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika humberger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar untuk menghilangka nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
